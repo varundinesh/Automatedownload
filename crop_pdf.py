1989a5ded5079db3d3350182b66e973520f64105
varun dineshan <varun4dinesh@gmail.com>
2020-01-17 11:50:53 -0500

    Update crop_pdf.py

--- crop_pdf.py
+++ crop_pdf.py
@@ -1,4 +1,6 @@
+#Python program to crop the pdf to different sizes.
 from PyPDF2 import PdfFileWriter, PdfFileReader
+#providing the path to the pdf
 reader=PdfFileReader(r"C:\\Users\\Varun\\Downloads\\maths\\final with dark bar.pdf","r")
 #reader.getNumPages()
 '''page=reader.getPage(0)
@@ -10,9 +12,11 @@ print(page.cropBox.getLowerRight())'''
 writer=PdfFileWriter()
 for i in range(reader.getNumPages()):
     page=reader.getPage(i)
+    #selecting the cordinate to begin the crop.
     page.cropBox.setLowerLeft((0,32))
     page.cropBox.setLowerRight((613, 32))
     writer.addPage(page)
+    #saving the final croped pdf to the directory.
     outstream=open(r"C:\\Users\\Varun\\Downloads\\maths\\withoutbar.pdf","wb")
     writer.write(outstream)
     outstream.close()
